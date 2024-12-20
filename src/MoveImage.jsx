a32b4b521d2806a26cc7ae2c0e49f32cc4e04ed7
gurunadh <gurunadh@gmail.com>
2024-12-20 10:24:01 +0530

    In progress

--- src/MoveImage.jsx
+++ src/MoveImage.jsx
@@ -6,27 +6,45 @@ function MoveImage() {
     const [position, setPosition] = useState({top: 0, left: 0});
     
     function handleMouseMove(event){
+        event.preventDefault();
+
+        if(isTouchDevice)
+            document.getElementsByTagName("body")[0].style.backgroundColor="orange";
+        else
+            document.getElementsByTagName("body")[0].style.backgroundColor="brown";
         setPosition({
             top: Math.random() * (window.innerHeight - event.target.offsetHeight),
             left: Math.random() * (window.innerWidth - event.target.offsetWidth)
         });
     }
 
+    function isTouchDevice(){
+        try {
+            document.createEvent("TouchEvent");
+            return true;
+        }
+        catch(e) {
+            return false;
+        }
+    }
     function handleTouchMove(event){
+        event.preventDefault();
         const touch = event.touches[0];
-        setPosition({
-            top: touch.clientY - event.target.offsetHeight / 2,
-            left: touch.clientX - event.target.offsetWidth / 2
-        })
+        
+        document.getElementsByTagName("body")[0].innerText = touch;
+        document.getElementsByTagName("body")[0].style.backgroundColor = "blue";
+        // setPosition({
+        //     top: touch.clientY - event.target.offsetHeight / 2,
+        //     left: touch.clientX - event.target.offsetWidth / 2
+        // })
     }
 
     return (
         <img 
-        tabIndex={-1}
         src={jerry}
         alt="moving image"
+        draggable="false"
         onMouseOver={handleMouseMove}
-        onTouchMove={handleTouchMove}
         style={{
             position: 'absolute',
             top: position.top,
